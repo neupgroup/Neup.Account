e0e52bf9daf66781b7f3a82010e6d6bdcdb60151
Kishor Neupane <heyneupkishor@gmail.com>
2025-09-05 18:25:42 +0000

    Functions cannot be passed directly to Client Components unless you expl

--- src/app/manage/people/page.tsx
+++ src/app/manage/people/page.tsx
@@ -17,28 +17,28 @@ export default async function PeopleSharingPage() {
     
     const sharingFeatures = [
         {
-            icon: Users,
+            iconName: "Users",
             title: "Family Sharing",
             description: "Manage your family group and shared subscriptions.",
             href: "/manage/people/family",
             show: canViewFamily,
         },
         {
-            icon: MailQuestion,
+            iconName: "MailQuestion",
             title: "Invitations",
             description: "Accept or reject requests from other users.",
             href: "/manage/people/invitations",
             show: canViewInvitations,
         },
         {
-            icon: Contact,
+            iconName: "Contact",
             title: "Contact Info Sharing",
             description: "Control how your contact information is shared.",
             href: "#",
             show: false, // Not implemented yet
         },
         {
-            icon: UserX,
+            iconName: "UserX",
             title: "Blocked Users",
             description: "See a list of people you've blocked.",
             href: "#",
